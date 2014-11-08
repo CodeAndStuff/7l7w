5aa911093e420e06c090e8e115ad266f6ddb0797
balhau <balhau@balhau.net>
2014-11-08 16:20:00 +0000

    Added hash tree exercise

--- ruby/day2.rb
+++ ruby/day2.rb
@@ -2,7 +2,30 @@
 
 #Open write file with and without code blocks
 
+#Class Tree
+
+class Tree
+  attr_accessor :children,:node_name
+
+  def initialize(hash={})
+    hash.each do |key,value|
+      @node_name = key
+      @children = value.map{|k,v| Tree.new(k=>v)}
+    end
+  end
+
+  def visit(&block)
+    block.call self
+  end
+
+  def visit_all(&block)
+    visit(&block)
+    children.each {|c| c.visit_all &block}
+  end
+end
 #Without code block
+
+
 
 def saveNoCodeBlock
   file=File::open("data/nocodeblock.txt","w")
@@ -32,6 +55,40 @@ def hashVsArray
   end
 end
 
+def arrayAsDeque
+  array=(1..10).to_a
+  until array.size ==0
+    puts array.shift
+  end
+end
+
+def array16NumbersEach
+  i=0
+  a=(1..16).to_a
+  a.each do |x|
+    if i<=3
+      puts a[i*4].to_s+","+a[i*4+1].to_s+","+a[i*4+2].to_s+","+a[i*4+3].to_s
+    end
+    i=i+1
+  end
+end
+
+def array16NumbersEachSlice
+  (1..16).each_slice(4) do |el|
+    p el
+  end
+end
+
 saveNoCodeBlock
 saveWithCodeBlock
 hashVsArray
+arrayAsDeque
+array16NumbersEach
+array16NumbersEachSlice
+
+treeHash={'grandpa' => { 'dad' => {'child 1' => {}, 'child 2' => {} },
+'uncle' => {'child 3' => {}, 'child 4' => {} } } }
+
+tree=Tree.new(treeHash)
+
+tree.visit_all{|t| p t.node_name}
