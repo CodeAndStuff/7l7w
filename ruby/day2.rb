06062f6cfeca34191cfef4a1861e5987f0fb781a
Balhau <balhau@balhau.net>
2014-11-08 12:40:50 +0000

    Added block/noblock and hash/array

--- ruby/day2.rb
+++ ruby/day2.rb
@@ -1,1 +1,37 @@
 #!/usr/bin/env ruby
+
+#Open write file with and without code blocks
+
+#Without code block
+
+def saveNoCodeBlock
+  file=File::open("data/nocodeblock.txt","w")
+  file << "Data in no code block mode\n"
+  file.close
+end
+
+#With code block
+
+def saveWithCodeBlock
+  File::open("data/codeblock.txt","w") do |file|
+    file << "Data in code block mode\n"
+  end
+end
+
+def hashVsArray
+  hash1={'pt'=>'Portugal','en'=>'England'}
+  array=hash1
+
+  array.each do |el|
+    puts el[0].to_s+"-"+el[1].to_s
+  end
+
+  hash2=Hash[array.map {|x| [x[0], x[1]]}]
+  hash2.each do |k,v|
+    puts k.to_s+"-"+v.to_s
+  end
+end
+
+saveNoCodeBlock
+saveWithCodeBlock
+hashVsArray
