34fcd551f634cfad3ab92ad01814ca9d89de036d
Balhau <balhau@balhau.net>
2014-11-09 10:06:09 +0000

    Adapted book code to use each blocks to iterate over csv rows

--- ruby/bookcode/ActAsCsvModule.rb
+++ ruby/bookcode/ActAsCsvModule.rb
@@ -1,6 +1,37 @@
 #!/usr/bin/env ruby
 #Example from the book
 
+class CsvRow
+
+  attr_accessor :row
+
+  def initialize(headers,row)
+    @headers=headers
+    @row=row
+  end
+
+  def method_missing name, *args
+    col=name.to_s
+    i=0
+    num=-1
+    pos=0
+    eq=false
+    while i < @headers.length
+      if @headers[i].to_s == col
+        num=i
+      end
+      i=i+1
+    end
+
+    if num!=-1
+      @row[num]
+    else
+      nil
+    end
+  end
+
+end
+
 module ActsAsCsv
 
   def self.included(base)
@@ -22,11 +53,17 @@ module ActsAsCsv
       @headers = file.gets.chomp.split(', ' )
 
       file.each do |row|
-        @csv_contents << row.chomp.split(', ' )
+        @csv_contents.push(CsvRow.new(@headers,row.chomp.split(',')))
       end
     end
 
     attr_accessor :headers, :csv_contents
+
+    def each(&block)
+      @csv_contents.each do |row|
+        block.call row
+      end
+    end
 
     def initialize
       read
@@ -43,3 +80,5 @@ end
 m = RubyCsv.new
 puts m.headers.inspect
 puts m.csv_contents.inspect
+
+m.each {|row| puts row.CSV}
